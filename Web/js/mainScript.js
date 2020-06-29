d381c9c523a36d6d2cab03c3cab48dfdd464e5dc
carrotpai <55906326+carrotpai@users.noreply.github.com>
2020-06-29 13:24:08 +0800

    Commit fix

--- Web/js/mainScript.js
+++ Web/js/mainScript.js
@@ -35,80 +35,81 @@ jQuery(document).ready(function($) {
 		alert("failed to get JSON");
 	});
 
-	setTimeout(() => console.log('Json загружен'), 500);
-
-
-	$.getJSON('/Web/data_pages/main_page/pizzas.json', function(data) {
-		var mas = data["pizzas"];
-		for (ind in mas){
-			labels[cur_ind].innerHTML = mas[ind]["label"];
-			prices[cur_ind].innerHTML = mas[ind]["price"];
-			descriptions[cur_ind].innerHTML = mas[ind]["description"];
-			$(images[cur_ind]).attr("src", "images/" + mas[ind]["name"] + ".png")
-			$(info[cur_ind]).attr('data-block', 'pizzas');
-			$(info[cur_ind]).attr('data-info', mas[ind]["name"]);
-			cur_ind++;
-		}
-	})
-	.fail(function(){
-		alert("failed to get JSON");
-	});
-
-	setTimeout(() => console.log('Json загружен'), 500);
-
-	$.getJSON('/Web/data_pages/main_page/snacks.json', function(data) {
-		var mas = data["snacks"];
-		for (ind in mas){
-			labels[cur_ind].innerHTML = mas[ind]["label"];
-			prices[cur_ind].innerHTML = mas[ind]["price"];
-			descriptions[cur_ind].innerHTML = mas[ind]["description"];
-			$(images[cur_ind]).attr("src", "images/" + mas[ind]["name"] + ".png")
-			$(info[cur_ind]).attr('data-block', 'snacks');
-			$(info[cur_ind]).attr('data-info', mas[ind]["name"]);
-			cur_ind++;
-		}
-	})
-	.fail(function(){
-		alert("failed to get JSON");
-	});
-
-	$.getJSON('/Web/data_pages/main_page/drinks.json', function(data) {
-		var mas = data["drinks"];
-		for (ind in mas){
-			labels[cur_ind].innerHTML = mas[ind]["label"];
-			prices[cur_ind].innerHTML = mas[ind]["price"];
-			descriptions[cur_ind].innerHTML = mas[ind]["description"];
-			$(images[cur_ind]).attr("src", "images/" + mas[ind]["name"] + ".png")
-			$(info[cur_ind]).attr('data-block', 'drinks');
-			$(info[cur_ind]).attr('data-info', mas[ind]["name"]);
-			cur_ind++;
-		}
-	})
-	.fail(function(){
-		alert("failed to get JSON");
-	});
-
-	setTimeout(() => console.log('Json загружен'), 500);
-
-	for(var i = 0; i < 15;i++){
-		buttons[i].addEventListener('click', (function(i){
-			return function(){
-				var d = $(prices[i]).text();
-				var prev = $('.total__price').text();
-				var sum = +prev + +d;
-				$('.total__price').text(sum);
-				localStorage.setItem('total__price', sum);
-				var a = $(info[i]).attr('data-block');
-				var b = $(info[i]).attr('data-info');
-				if (localStorage.getItem(b)){
-					let c = JSON.parse(localStorage.getItem(b));
-					console.log(c[1]);
-					var cur_count = c[1] + 1;
-					localStorage.setItem(b, JSON.stringify([a, cur_count]));
-				} else{
-					localStorage.setItem(b, JSON.stringify([a, 1]));
-				}
+	setTimeout(function(){
+		$.getJSON('/Web/data_pages/main_page/pizzas.json', function(data) {
+			var mas = data["pizzas"];
+			for (ind in mas){
+				labels[cur_ind].innerHTML = mas[ind]["label"];
+				prices[cur_ind].innerHTML = mas[ind]["price"];
+				descriptions[cur_ind].innerHTML = mas[ind]["description"];
+				$(images[cur_ind]).attr("src", "images/" + mas[ind]["name"] + ".png")
+				$(info[cur_ind]).attr('data-block', 'pizzas');
+				$(info[cur_ind]).attr('data-info', mas[ind]["name"]);
+				cur_ind++;
 			}
-		})(i))
-	}
+		})
+		.fail(function(){
+			alert("failed to get JSON");
+		});
+
+		setTimeout(function(){
+			$.getJSON('/Web/data_pages/main_page/snacks.json', function(data) {
+				var mas = data["snacks"];
+				for (ind in mas){
+					labels[cur_ind].innerHTML = mas[ind]["label"];
+					prices[cur_ind].innerHTML = mas[ind]["price"];
+					descriptions[cur_ind].innerHTML = mas[ind]["description"];
+					$(images[cur_ind]).attr("src", "images/" + mas[ind]["name"] + ".png")
+					$(info[cur_ind]).attr('data-block', 'snacks');
+					$(info[cur_ind]).attr('data-info', mas[ind]["name"]);
+					cur_ind++;
+				}
+			})
+			.fail(function(){
+				alert("failed to get JSON");
+			});
+
+			setTimeout(function(){
+				$.getJSON('/Web/data_pages/main_page/drinks.json', function(data) {
+					var mas = data["drinks"];
+					for (ind in mas){
+						labels[cur_ind].innerHTML = mas[ind]["label"];
+						prices[cur_ind].innerHTML = mas[ind]["price"];
+						descriptions[cur_ind].innerHTML = mas[ind]["description"];
+						$(images[cur_ind]).attr("src", "images/" + mas[ind]["name"] + ".png")
+						$(info[cur_ind]).attr('data-block', 'drinks');
+						$(info[cur_ind]).attr('data-info', mas[ind]["name"]);
+						cur_ind++;
+					}
+				})
+				.fail(function(){
+					alert("failed to get JSON");
+				});
+
+				setTimeout(function(){
+					for(var i = 0; i < 15;i++){
+						buttons[i].addEventListener('click', (function(i){
+							return function(){
+								var d = $(prices[i]).text();
+								var prev = $('.total__price').text();
+								var sum = +prev + +d;
+								$('.total__price').text(sum);
+								localStorage.setItem('total__price', sum);
+								var a = $(info[i]).attr('data-block');
+								var b = $(info[i]).attr('data-info');
+								if (localStorage.getItem(b)){
+									let c = JSON.parse(localStorage.getItem(b));
+									console.log(c[1]);
+									var cur_count = c[1] + 1;
+									localStorage.setItem(b, JSON.stringify([a, cur_count]));
+								} else{
+									localStorage.setItem(b, JSON.stringify([a, 1]));
+								}
+							}
+						})(i))
+					}
+				}, 500);
+			}, 500);
+		}, 500);
+	}, 500);
 });
